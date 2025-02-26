cfd9d038db0c5a91c2abdacf4500eb33b79c188f
Andrés Romero <150357245+aarzec@users.noreply.github.com>
2025-02-25 22:50:28 -0500

    Add project files.

---
+++ Controllers/DocumentoController.cs
@@ -0,0 +1,25 @@
+﻿using capaEntidad;
+using capaNegocio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeraAppNetCoreMVC.Controllers
+{
+    public class DocumentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PruebaUso()
+        {
+            return View();
+        }
+
+        public List<DocumentoCLS> listarDocumentos()
+        {
+            DocumentoBL obj = new DocumentoBL();
+            return obj.listarDocumentos();
+        }
+    }
+}
---
+++ wwwroot/js/documento.js
@@ -0,0 +1,12 @@
+﻿window.onload = function () {
+    listarDocumentos();
+}
+
+async function listarDocumentos() {
+
+    pintar({
+        url: "Documento/listarDocumentos",
+        cabeceras: ["Id Documento", "Nombre", "Bhabilitado"],
+        propiedades: ["idTipoDocumento", "nombre", "bhabilitado"]
+    })
+}
